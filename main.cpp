0b2ec6d8afc8dda21d76bb1cd2a9b247262ed556
jrp109-uwf <jrp109@students.uwf.edu>
2026-09-13 22:15:39 -0500

    I added the main loop that shows off the parsing for part 1 of the project.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
+#include "param.hpp"
 #include "parse.hpp"
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <cstring>
 
-int main() {
-    
+using namespace std;
+
+bool debug = false;
+
+int main(int argc, char *argv[]) {
+    // -Debug flag check
+    for (int i = 1; i < argc; i++) {
+        // I could've used strcmp(...) == 0 instead of string_view() but idk modern features are nice
+        if (string_view(argv[i]) == "-Debug") {
+            debug = true;
+        }
+    }
+
+    cout << "myshell - part 1" << endl;
+
+    // This is just a temporary debug mode indicator for part 1
+    if (debug) {
+        cout << "Debug mode is on." << endl;
+    }
+    else {
+        cout << "Debug mode is off." << endl;
+    }
+
+    Parser parser;
+    string input;
+    char *inputbuf = nullptr;
+
+    // Main loop
+    while (true) {
+        cout << ">>>: ";
+        getline(cin, input);
+
+        // Check before its parsed
+        if (input == "exit") break;
+        
+        // A set size char[] would have been just fine, but I went this route so nothing gets truncated.
+        inputbuf = new char[input.length() + 1];
+        strcpy(inputbuf, input.c_str());
+
+        Param param = parser.parse(inputbuf);
+        if (debug) param.printParams();
+
+        delete[] inputbuf;
+        inputbuf = nullptr;
+    }
 }
