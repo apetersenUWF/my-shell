b15177804ffe1c273e30510929fd8b5f1dd29259
acp58 <acp58@cs-ssh.argo.uwf.edu>
2026-09-17 15:56:51 -0500

    Add I/O redirect

--- myshell.cpp
+++ myshell.cpp
@@ -12,6 +12,22 @@ using namespace std;
 
 bool debug = false;
 
+void redirectOutput(const char* filename) {
+    if (filename == nullptr) return;
+    if (freopen(filename, "w", stdout) == nullptr) { //freopen redirects stdout or stdin to the provided filename, upon failure it returns nullptr
+        perror("freopen failed to redirect stdin");
+        _exit(EXIT_FAILURE);
+    }
+}
+
+void redirectInput(const char* filename) {
+    if (filename == nullptr) return;
+    if (freopen(filename, "r", stdin) == nullptr) {
+    perror("freopen failed to redirect stdout");
+    _exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char *argv[]) {
     // -Debug flag check
     for (int i = 1; i < argc; i++) {
@@ -54,8 +70,16 @@ int main(int argc, char *argv[]) {
             //Must be the child process
             //I used execvp here because it takes the vector of arguments from the Param class cleanly and automatically searches the system to locate the program rather than requiring the path as an argument
             //execvp requires the final element of the argument vector to be null, meaning this will work up to MAXARGS - 1 arguments
-            execvp(args[0], args); 
-            perror("execvp failed");
+            if (param.getOutputRedirect() != nullptr) { //redirect output
+                redirectOutput(param.getOutputRedirect());
+            }
+            if (param.getInputRedirect() != nullptr) {//redirect input
+                redirectInput(param.getInputRedirect());
+            }
+            execvp(args[0], args);  //searches system and executes the arguments specified by args
+                                    //execvp should never return here because it was transformed into the new process
+            perror("execvp failed");//if execvp returns it must have failed, print error message and terminate child
+            _exit(EXIT_FAILURE);
         }
         else {
             //Must be the parent process
